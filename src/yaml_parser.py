443b93dad85cdd39e1db73b7f06ce76ba14b0abb
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-11-09 17:27:06 +0300

    refactoring, add code generators methods

--- src/yaml_parser.py
+++ src/yaml_parser.py
@@ -1,16 +1,21 @@
 from dataclasses import dataclass, field
 from os import PathLike
-from typing import Any, Optional
+from typing import Any, Optional, Callable, TYPE_CHECKING
 
 import yaml
 
+from src.codegen_tools import ABCExpressionTranslator, generate_code, BlockCode
+from src.lexer import tokenize, Token
+
+
 __all__ = ["Info", "SchemaAttribute", "Schema", "parse_config"]
+
+CODEGEN_STRATEGY = Callable[[list[Token], ABCExpressionTranslator], BlockCode]
 
 
 @dataclass
 class Info:
     """meta information"""
-
     id: str
     name: str
     author: str
@@ -26,6 +31,14 @@ class SchemaAttribute:
     doc: Optional[str]
     raw_code: str
 
+    def generate_code(self,
+                      translator: ABCExpressionTranslator,
+                      code_generator: Optional[CODEGEN_STRATEGY] = None):
+        code_generator = code_generator or generate_code
+
+        tokens = tokenize(self.raw_code)
+        return code_generator(tokens, translator)
+
 
 @dataclass
 class Schema:
@@ -34,7 +47,36 @@ class Schema:
     pre_validate_code: Optional[str]
     split_code: Optional[str]
     doc: Optional[str]
+    view_keys: list[str]
     attrs: list[SchemaAttribute] = field(default_factory=list)
+
+    @property
+    def aliases(self) -> dict[str, str]:
+        return {a.name: a.alias for a in self.attrs if a.alias}
+
+    @property
+    def attrs_names(self) -> list[str]:
+        return [a.name for a in self.attrs]
+
+    def generate_pre_validate_code(self,
+                                   translator: ABCExpressionTranslator,
+                                   code_generator: Optional[CODEGEN_STRATEGY] = None):
+        code_generator = code_generator or generate_code
+
+        if self.pre_validate_code:
+            tokens = tokenize(self.pre_validate_code)
+            return code_generator(tokens, translator)
+        return ""
+
+    def generate_split_code(self,
+                            translator: ABCExpressionTranslator,
+                            code_generator: Optional[CODEGEN_STRATEGY] = None
+                            ):
+        code_generator = code_generator or generate_code
+        if self.split_code:
+            tokens = tokenize(self.split_code)
+            return code_generator(tokens, translator)
+        return ""
 
 
 def _parse_class(class_name: str, content: dict[str:Any]) -> Schema:
@@ -51,6 +93,7 @@ def _parse_class(class_name: str, content: dict[str:Any]) -> Schema:
         split_code=steps.get("split", None),
         doc=content.get("doc"),
         name=class_name,
+        view_keys=content.get("steps").get("view")
     )
 
     for attr in parser_attrs:
