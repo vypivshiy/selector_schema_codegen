f3401b46e90bedc030b46cf111a5b9360d2cf41e
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-11-23 08:47:34 +0300

    remove -n arg, change python to python.bs4, python.parsel

--- ssc_codegen/cli.py
+++ ssc_codegen/cli.py
@@ -14,24 +14,19 @@ def _parse_arguments():
 
     parser.add_argument("cfg", help="YAML config file path")
     parser.add_argument(
-        "lang", choices=["python", "dart"], help="Programming language choice"
+        "lang", choices=["python.bs4", "python.parsel", "dart"], help="Programming language and lib choice"
     )
-    parser.add_argument(
-        "-n",
-        "--name",
-        default="schema",
-        help="schema filename output " "(default: schema + .extension)",
-    )
+
     parser.add_argument(
         "-o",
         "--out",
-        default=Path.cwd(),
-        help="Output directory (default: current working directory)",
+        default=f"{Path.cwd()}/schema.{{}}",
+        help="Output directory (default: current working directory/schema.<ext>)",
     )
     parser.add_argument(
         "-y",
         dest="OVERWRITE",
-        default=Path.cwd(),
+        default=False,
         action="store_true",
         help="Suggest overwrite file",
     )
@@ -49,50 +44,54 @@ def main():
     cfg_file = args.cfg
     programming_lang = args.lang
     output_dir = args.out
-    output_filename = args.name
 
-    if output_filename == "schema":
+    if output_dir.endswith("schema.{}"):
         match programming_lang:
-            case "python":
-                output_filename += ".py"
+            case "python.bs4" | "python.parsel":
+                output_dir = output_dir.format(".py")
             case "dart":
-                output_filename += ".dart"
+                output_dir = output_dir.format(".dart")
+
             case _:
                 print("Not supported, exit")
                 exit(1)
-    else:
-        output_filename = output_dir / Path(output_filename)
+
     print(
         "Start generate code:",
         f"Config file: {cfg_file}",
         f"Prog lang: {programming_lang}",
-        f"Output dir: {output_dir}/{output_filename}",
+        f"Output: {output_dir}",
         sep="\n",
     )
-    if Path(f"{output_dir}/{output_filename}").exists() and not args.OVERWRITE:
+    if Path(output_dir).exists() and not args.OVERWRITE:
         choice = input("Overwrite? (y/n)?")
         if choice.lower() != "y":
             print("Cancel operation")
             exit(1)
 
     match programming_lang:
-        case "python":
+        case "python.parsel":
             from ssc_codegen.configs.python.python_parsel import Translator
+
+            template = "python/python_any.j2"
+
+        case "python.bs4":
+            from ssc_codegen.configs.python.python_bs4 import Translator
 
             template = "python/python_any.j2"
         case "dart":
             from ssc_codegen.configs.dart.dart_html import Translator
-
             template = "dart/dart_html.j2"
         case _:
             print("Unknown prog lang, exit")
             exit(1)
+
     print("Generate code")
     cfg_info = parse_config(file=cfg_file, translator=Translator)
     code = generate_code(cfg_info, template)
 
-    print(f"Write to {output_filename}")
-    with open(output_filename, "w") as f:
+    print(f"Write to {output_dir}")
+    with open(output_dir, "w") as f:
         f.write(code)
     print("Done")
 
