3f948889180b22175fc7aea7c1b4fe3747aff9c3
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-02-12 17:37:45 +0300

    change render_code signature args to BaseStructStrategy, add Dict, List support

--- ssc_codegen/template.py
+++ ssc_codegen/template.py
@@ -2,13 +2,32 @@ from typing import TYPE_CHECKING
 
 import jinja2
 
+from ssc_codegen.structs import ItemStruct, DictStruct, ListStruct, BaseStructStrategy
+from ssc_codegen.generator import StructParser
 if TYPE_CHECKING:
-    from generator import StructParser, CodeConverter
+    from generator import CodeConverter
 
 ENV = jinja2.Environment(
     loader=jinja2.PackageLoader("ssc_codegen.templates.python.parsel", ""))
 
 
-def render_code(code_converter: "CodeConverter", *struct: "StructParser"):
+def render_code(code_converter: "CodeConverter", *structs: "BaseStructStrategy") -> str:
     base_tmp = ENV.get_template('baseStruct.j2').render()
-    return base_tmp + "\n\n" + ENV.get_template('itemStruct.j2').render(struct=struct, converter=code_converter)
+    codes: list[str] = []
+    for st in structs:
+        if isinstance(st, ItemStruct):
+            st = StructParser(instance=st)
+            codes.append(
+                ENV.get_template('itemStruct.j2').render(struct=st, converter=code_converter)
+            )
+        elif isinstance(st, DictStruct):
+            st = StructParser(instance=st)
+            codes.append(
+                ENV.get_template('dictStruct.j2').render(struct=st, converter=code_converter)
+            )
+        elif isinstance(st, ListStruct):
+            st = StructParser(instance=st)
+            codes.append(
+                ENV.get_template('listStruct.j2').render(struct=st, converter=code_converter)
+            )
+    return base_tmp + "\n\n".join(codes)
