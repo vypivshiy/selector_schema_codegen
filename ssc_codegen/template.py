bbb4d8ace1f185ad8b283590615ad9b52cd3482c
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-02-14 15:28:50 +0300

    add camelCase, snake_case, schema_signature macro

--- ssc_codegen/template.py
+++ ssc_codegen/template.py
@@ -1,16 +1,50 @@
+import re
 from typing import TYPE_CHECKING
+import json
 
 import jinja2
 
+from ssc_codegen.objects import VariableState
 from ssc_codegen.schemas import ItemSchema, DictSchema, ListSchema, BaseSchemaStrategy
 from ssc_codegen.generator import StructParser
 if TYPE_CHECKING:
     from generator import CodeConverter
 
 
+def camelcase(s: str) -> str:
+    return "".join(word[0].upper() + word[1:] for word in s.split("_"))
+
+
+def snake_case(s: str) -> str:
+    return re.sub(r"(?<!^)(?=[A-Z])", "_", s).lower()
+
+
+def schema_lines_signature(st: "StructParser", prefix: str = "", end: str = '\n') -> str:
+    """representation object signature for docstring i"""
+    json_like = {}
+    _types = {
+        VariableState.NONE: "null",
+        VariableState.STRING: "String",
+        VariableState.LIST_STRING: "Array['String']"
+    }
+    for method in st.methods:
+        node = method.ast[0]
+        json_like[method.name] = _types.get(node.return_arg_type)
+    object_signature = json.dumps(json_like, indent=4).split('\n')
+    header: str = f"{st.name} view() item signature:"
+    return end.join(f'{prefix}{line}' for line in [header, ""] + object_signature)
+
+
 def render_code(code_converter: "CodeConverter", *schemas: "BaseSchemaStrategy") -> str:
     jinja_env = jinja2.Environment(
         loader=jinja2.PackageLoader(code_converter.templates_path, ""))
+
+    jinja_env.globals.update(
+        toCamel=camelcase,
+        to_snake=snake_case,
+        schema_doc_signature=schema_lines_signature
+    )
+
     base_tmp = jinja_env.get_template('baseStruct.j2').render()
     codes: list[str] = []
     for st in schemas:
