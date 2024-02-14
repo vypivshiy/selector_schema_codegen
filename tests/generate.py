332a82a32de0f6e6ab1b17f7209f108af54372f7
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-02-14 15:27:03 +0300

    add dart generate

--- tests/generate.py
+++ tests/generate.py
@@ -1,3 +1,4 @@
+# TODO remove
 import pathlib
 
 from ssc_codegen.template import render_code
@@ -11,10 +12,14 @@ if __name__ == '__main__':
     from ssc_codegen.converters.py_bs4 import converter as bs4_converter
     from ssc_codegen.converters.py_selectolax import converter as slax_converter
     from ssc_codegen.converters.py_scrapy import converter as scrapy_converter
-
+    from ssc_codegen.converters.dart_universal_html import converter as dart_converter
     pathlib.Path("parsers/books_parsel.py").write_text(render_code(parsel_converter, Book(), BooksCatalogue()))
     pathlib.Path("parsers/books_bs4.py").write_text(render_code(bs4_converter, Book(), BooksCatalogue()))
     pathlib.Path("parsers/books_slax.py").write_text(render_code(slax_converter, Book(), BooksCatalogue()))
     pathlib.Path("parsers/books_scrapy.py").write_text(render_code(scrapy_converter, Book(), BooksCatalogue()))
 
     os.system('black parsers/books_parsel.py parsers/books_bs4.py parsers/books_slax.py parsers/books_scrapy.py')
+
+    # dart
+    pathlib.Path("../test_dart/test/schema.dart").write_text(render_code(dart_converter, Book(), BooksCatalogue()))
+    os.system("dart format ../test_dart/test/schema.dart")
