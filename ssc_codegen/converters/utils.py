5accdb115830226fc9f836006a504dd8787d5090
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-02-15 16:14:59 +0300

    fix wrap regex quotas for \" case

--- ssc_codegen/converters/utils.py
+++ ssc_codegen/converters/utils.py
@@ -11,6 +11,14 @@ def sanitize_regex(s: str, escape_ch: str = "\\") -> str:
 
 
 def wrap_regex_pattern(s: str, prefix: str = "r", escape_ch: str = "\\") -> str:
+    if escape_ch + '"' in s:
+        s = s.replace(escape_ch + '"', '"')
+
     if '"' in s:
         s = s.replace('"', escape_ch + '"')
     return prefix + '"' + s + '"'
+
+
+if __name__ == '__main__':
+    # r"content=\"(https://.*\.m3u8)\""
+    print(wrap_regex_pattern(r"content=\"(https://.*\.m3u8)\""))
