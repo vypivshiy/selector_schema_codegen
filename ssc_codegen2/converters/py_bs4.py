1c768cd6eba5a8f78885e98d068a9ae060b5a3d3
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-05-17 15:50:48 +0300

    refactoring templates

---
+++ ssc_codegen2/converters/py_bs4.py
@@ -0,0 +1,28 @@
+from ssc_codegen2.converters.generator import CodeGenerator
+from ssc_codegen2.converters.python import PythonCodeConverter
+from ssc_codegen2.tokens import TokenType
+
+converter = PythonCodeConverter()
+
+XPATH_ERR_MSG = "bs4 not support xpath"
+
+
+@converter(TokenType.OP_XPATH)
+def op_xpath(_):
+    raise NotImplementedError("bs4 not support xpath")
+
+
+@converter(TokenType.OP_XPATH_ALL)
+def op_xpath_all(_):
+    raise NotImplementedError("bs4 not support xpath")
+
+
+@converter(TokenType.OP_ASSERT_XPATH)
+def op_assert_xpath(_):
+    raise NotImplementedError("bs4 not support xpath")
+
+
+code_generator = CodeGenerator(
+    templates_path='ssc_codegen2.converters.templates.py',
+    base_struct_path='bs4',
+    converter=converter)
