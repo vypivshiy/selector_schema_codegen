6e73f72eb48d7dc4036ce608b6d50b4b570cc4c6
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-06-03 07:47:25 +0300

    rename sanitize_regex to escape_str. change signature

--- ssc_codegen/converters/utils.py
+++ ssc_codegen/converters/utils.py
@@ -1,13 +1,13 @@
 import re
 
 
-def sanitize_regex(s: str, escape_ch: str = "\\") -> str:
-    """sanitize matching characters. Used, for trim, l_trim, r_trim realisations by regex"""
+def escape_str(s: str, pattern: str = r"[\-.^$*+?{}\[\]\\|()]", escape_ch: str = "\\") -> str:
+    """Sanitize matching characters. Used, for trim, l_trim, r_trim realizations by regex"""
 
     def _repl(ch: re.Match[str]) -> str:
         return escape_ch + ch[0]
 
-    return re.sub(r"[\-.^$*+?{}\[\]\\|()]", _repl, s)
+    return re.sub(pattern, _repl, s)
 
 
 def to_camelcase(s: str) -> str:
