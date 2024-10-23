6ae5552a072ff311af5f2eef4f8bc5f37be0ba9d
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-10-23 16:02:18 +0300

    escape_str func

--- ssc_codegen/ssc2/converters/utils.py
+++ ssc_codegen/ssc2/converters/utils.py
@@ -16,3 +16,15 @@ def to_lower_camel_case(s: str):
 def wrap_double_quotes(s: str, escape_ch: str = "\\") -> str:
     """used if string marks only in this chars"""
     return '"' + s.replace('"', escape_ch + '"') + '"'
+
+
+def escape_str(
+    s: str, pattern: str = r"[\-.^$*+?{}\[\]\\|()]", escape_ch: str = "\\"
+) -> str:
+    """Sanitize matching characters.
+    Used, for trim, l_trim, r_trim realizations by regex"""
+
+    def _repl(ch: re.Match[str]) -> str:
+        return escape_ch + ch[0]
+
+    return re.sub(pattern, _repl, s)
