2aac5988604f7aa1e48b6ad33a9dc5a5561c42eb
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-12-23 22:23:52 +0300

    bump

--- ssc_codegen/__init__.py
+++ ssc_codegen/__init__.py
@@ -2,7 +2,7 @@ from .document import HTMLDocument, StringDocument, ArrayDocument, AssertDocumen
     NumericDocument
 from .schema import ItemSchema, DictSchema, ListSchema, FlatListSchema
 
-VERSION = "0.4.0"
+VERSION = "0.4.1"
 
 
 class Document(HTMLDocument, StringDocument, ArrayDocument, AssertDocument, DefaultDocument, NumericDocument):
