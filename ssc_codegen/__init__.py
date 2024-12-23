a9348c75a91ed4c57c46a3f2c1f7c54a6bc8f2bf
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-12-23 19:03:00 +0300

    bump

--- ssc_codegen/__init__.py
+++ ssc_codegen/__init__.py
@@ -1,7 +1,7 @@
 from .document import HTMLDocument, StringDocument, ArrayDocument, AssertDocument, NestedDocument, DefaultDocument
 from .schema import ItemSchema, DictSchema, ListSchema, FlatListSchema
 
-VERSION = "0.4.0b"
+VERSION = "0.4.0"
 
 
 class Document(HTMLDocument, StringDocument, ArrayDocument, AssertDocument, DefaultDocument):
@@ -13,12 +13,17 @@ class Nested(HTMLDocument, NestedDocument, ArrayDocument, AssertDocument):
 
 
 def D() -> Document:  # noqa
+    """Shortcut as a Document() object"""
     return Document()
 
 
 def N() -> Nested:  # noqa
+    """Shortcut as a Nested() object"""
     return Nested()
 
 
 def R() -> Document: # noqa
+    """Shortcut as a Document().raw() object.
+    For regex and format string operations
+    """
     return D().raw()
