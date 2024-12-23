da07f96e74267c4c4bea037d94c8969f7b407994
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-12-23 20:51:34 +0300

    BaseCodeConverter.convert: ignore append acc output if string empty

--- ssc_codegen/converters/base.py
+++ ssc_codegen/converters/base.py
@@ -58,32 +58,48 @@ class BaseCodeConverter:
     ) -> list[str]:
         """convert ast to code parts"""
         acc = acc or []
+        # in c-like languages docstring top on struct/class/method/function...
         if ast_entry.kind == TokenType.STRUCT and getattr(
             ast_entry, "docstring_class_top", False
         ):
             ast_entry: StructParser
-            acc.append(self._pre_convert_node(ast_entry.doc))
-            acc.append(self._post_convert_node(ast_entry.doc))
+            if pre_code := self._pre_convert_node(ast_entry.doc):
+                acc.append(pre_code)
+            if post_code := self._post_convert_node(ast_entry.doc):
+                acc.append(post_code)
 
-        acc.append(self._pre_convert_node(ast_entry))
+
+        # will be converted later
+        if ast_entry.kind != TokenType.EXPR_DEFAULT:
+            if pre_code := self._pre_convert_node(ast_entry):
+                acc.append(pre_code)
+
         if ast_entry.kind == TokenType.STRUCT:
             ast_entry: StructParser
             if not ast_entry.docstring_class_top:
-                acc.append(self._pre_convert_node(ast_entry.doc))
-                acc.append(self._post_convert_node(ast_entry.doc))
+                if pre_code := self._pre_convert_node(ast_entry.doc):
+                    acc.append(pre_code)
+                if post_code := self._post_convert_node(ast_entry.doc):
+                    acc.append(post_code)
 
-            acc.append(self._pre_convert_node(ast_entry.init))
-            acc.append(self._post_convert_node(ast_entry.init))
+            if pre_code := self._pre_convert_node(ast_entry.init):
+                acc.append(pre_code)
+            if post_code := self._post_convert_node(ast_entry.init):
+                acc.append(post_code)
         if getattr(ast_entry, "body", None):
-            if ast_entry.kind == TokenType.STRUCT_FIELD and ast_entry.default:
-                acc.append(self._pre_convert_node(ast_entry.default))
-
+            if ast_entry.kind == TokenType.STRUCT_FIELD and ast_entry.default:  # noqa
+                if pre_code := self._pre_convert_node(ast_entry.default):  # noqa
+                    acc.append(pre_code)
             for ast_node in ast_entry.body:
                 self.convert(ast_node, acc)
 
-            if ast_entry.kind == TokenType.STRUCT_FIELD and ast_entry.default:
-                acc.append(self._post_convert_node(ast_entry.default))
-        acc.append(self._post_convert_node(ast_entry))
+            if ast_entry.kind == TokenType.STRUCT_FIELD and ast_entry.default:  # noqa
+                if post_code := self._post_convert_node(ast_entry.default):
+                    acc.append(post_code)
+        # will be converted later
+        if ast_entry.kind != TokenType.EXPR_DEFAULT:
+            if post_code := self._post_convert_node(ast_entry):
+                acc.append(post_code)
         return acc
 
 
