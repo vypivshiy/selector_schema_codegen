009c87de68c3209d27f2a241d7005595a3a3abc5
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-12-23 21:04:59 +0300

    update example

--- examples/schemas/booksToScrape.py
+++ examples/schemas/booksToScrape.py
@@ -1,14 +1,9 @@
 """Dummy parser config for http://books.toscrape.com/"""
 
-from ssc_codegen.document import D, N
-from ssc_codegen.schema import (
-    ItemSchema,
-    ListSchema,
-    FlattenListSchema,
-)
+from ssc_codegen import D, N, ItemSchema, ListSchema, FlatListSchema
 
 
-class Urls(FlattenListSchema):
+class Urls(FlatListSchema):
     """fetch add patches and urls from <a> tag"""
 
     __SPLIT_DOC__ = D().css_all("a")
@@ -22,12 +17,12 @@ class Books(ListSchema):
     image_url = D().css(".thumbnail").attr("src")
     url = D().css(".image_container > a").attr("href")
     rating = D().css(".star-rating").attr("class").ltrim("star-rating ")
-    price = D().default("0").css(".price_color").text().re(r"\d+")
+    price = D().default(0).css(".price_color").text().re(r"(\d+)").to_int()
 
 
 class CataloguePage(ItemSchema):
-    __PRE_VALIDATE__ = D().css("title").text().assert_re(r"Books to Scrape")
+    __PRE_VALIDATE__ = D().css("title").text().is_regex(r"Books to Scrape")
 
-    title = D().css("title").text()
-    urls: Urls = N().sub_parser(Urls)
-    books: Books = N().sub_parser(Books)
+    title = D().css("title").text().re_sub(r'^\s+', '').re_sub(r'\s+$')
+    urls = N().sub_parser(Urls)
+    books = N().sub_parser(Books)
