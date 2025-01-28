091e33dea8ab7bc3df55a0ea3968d9317bb360d4
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-01-28 12:05:26 +0300

    add utils tests

---
+++ tests/utils/test_convert_query.py
@@ -0,0 +1,24 @@
+import pytest
+
+from ssc_codegen.selector_utils import (
+    css_to_xpath,
+    xpath_to_css,
+)
+
+
+@pytest.mark.parametrize(
+    'q,out',
+    [('title', '//title'),
+('head > title', '//head/title')
+     ])
+def test_convert_css_to_xpath(q:str, out:str) -> None:
+    assert css_to_xpath(q, prefix='//') == out
+
+
+@pytest.mark.parametrize(
+    'q,out',
+    [('//title', 'title'),
+('//head/title', 'head > title')
+     ])
+def test_convert_xpath_to_css(q:str, out:str) -> None:
+    assert xpath_to_css(q) == out
