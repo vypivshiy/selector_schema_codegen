c63070ac0f333d05205a581f31fce750de89a0e6
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-01-31 09:31:24 +0300

    generator all schemas script

---
+++ examples/generate_code.sh
@@ -0,0 +1,4 @@
+ssc-gen go schemas/booksToScrape.py -o go --package main
+ssc-gen py schemas/booksToScrape.py -i parsel -o python
+ssc-gen js schemas/booksToScrape.py -o js
+ssc-gen dart schemas/booksToScrape.py -o dart
