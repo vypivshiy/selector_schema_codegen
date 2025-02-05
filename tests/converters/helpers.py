f9aa28c64cf53600b2a94c9234f67fb86bde68af
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-02-06 01:29:17 +0300

    add test converters implementation status

---
+++ tests/converters/helpers.py
@@ -0,0 +1,58 @@
+from importlib.resources import read_text
+
+from ssc_codegen.converters.base import BaseCodeConverter
+from ssc_codegen.tokens import TokenType
+
+# reserved tokens, not used in converters
+EXCLUDE_TOKENS = {
+    TokenType.VARIABLE,
+    # in ast build step converts to TokenType.EXPR_DEFAULT_START and TokenType.EXPR_DEFAULT_END
+    TokenType.EXPR_DEFAULT,
+    TokenType.MODULE,  # first node
+    # used inner TokenType.STRUCT_PARSE_START node
+    # and converts to call parse methods for fields
+    TokenType.STRUCT_CALL_FUNCTION,
+    # used inner TokenType.TYPEDEF node
+    # and converts to types
+    TokenType.TYPEDEF_FIELD,
+}
+
+# situational tokens
+# impl, if target language allowed typing
+TYPING_TOKENS_CONVERT_IMPL = {
+    TokenType.TYPEDEF,
+}
+
+# impl, if target language and backend supports css selections
+CSS_TOKENS_IMPL = {
+    TokenType.EXPR_CSS,
+    TokenType.EXPR_CSS_ALL,
+    TokenType.IS_CSS,
+}
+# impl, if target language and backend supports xpath selections
+XPATH_TOKENS_IMPL = {
+    TokenType.EXPR_XPATH,
+    TokenType.EXPR_XPATH_ALL,
+    TokenType.IS_XPATH,
+}
+
+
+REQUIRED_TOKENS_CONVERT_IMPL = {
+    t
+    for t in TokenType
+    if t
+    not in TYPING_TOKENS_CONVERT_IMPL
+    | CSS_TOKENS_IMPL
+    | XPATH_TOKENS_IMPL
+    | EXCLUDE_TOKENS
+}
+
+
+def new_converter_check(
+        name: str,
+        converter: BaseCodeConverter,
+        include: set[TokenType],
+        exclude: set[TokenType] | None = None) -> tuple[str, BaseCodeConverter, set[TokenType], set[TokenType]]:
+    exclude = exclude or set()
+    return name, converter, include, exclude
+    
---
+++ tests/converters/test_buildin_converters.py
@@ -0,0 +1,121 @@
+"""helper function helps check converter implementation"""
+import pytest
+from helpers import (
+    CSS_TOKENS_IMPL,
+    REQUIRED_TOKENS_CONVERT_IMPL,
+    TYPING_TOKENS_CONVERT_IMPL,
+    XPATH_TOKENS_IMPL,
+    new_converter_check,
+)
+
+from ssc_codegen.converters.base import BaseCodeConverter
+
+# dart
+from ssc_codegen.converters.dart_universal_html import (
+    converter as dart_universal_html,
+)
+
+# go
+from ssc_codegen.converters.go_goquery import converter as go_goquery
+
+# js
+from ssc_codegen.converters.js_pure import converter as js_pure
+
+# python
+from ssc_codegen.converters.py_bs4 import converter as py_bs4
+from ssc_codegen.converters.py_parsel import converter as py_parsel
+from ssc_codegen.converters.py_scrapy import converter as py_scrapy
+from ssc_codegen.converters.py_selectolax import converter as py_selectolax
+from ssc_codegen.tokens import TokenType
+
+_TEST_BS4 = new_converter_check(
+    "py_bs4",
+    converter=py_bs4,
+    include=REQUIRED_TOKENS_CONVERT_IMPL
+    | CSS_TOKENS_IMPL
+    | TYPING_TOKENS_CONVERT_IMPL,
+)
+
+_TEST_PARSEL = new_converter_check(
+    "py_parsel",
+    converter=py_parsel,
+    include=REQUIRED_TOKENS_CONVERT_IMPL
+    | CSS_TOKENS_IMPL
+    | XPATH_TOKENS_IMPL
+    | TYPING_TOKENS_CONVERT_IMPL,
+)
+
+_TEST_SELECTOLAX = new_converter_check(
+    "py_selectolax",
+    converter=py_selectolax,
+    include=REQUIRED_TOKENS_CONVERT_IMPL
+    | CSS_TOKENS_IMPL
+    | TYPING_TOKENS_CONVERT_IMPL,
+)
+_TEST_SCRAPY = new_converter_check(
+    "py_scrapy",
+    converter=py_scrapy,
+    include=REQUIRED_TOKENS_CONVERT_IMPL
+    | CSS_TOKENS_IMPL
+    | XPATH_TOKENS_IMPL
+    | TYPING_TOKENS_CONVERT_IMPL,
+)
+_TEST_DART = new_converter_check(
+    "dart_universal_html",
+    converter=dart_universal_html,
+    include=REQUIRED_TOKENS_CONVERT_IMPL
+    | CSS_TOKENS_IMPL
+    | TYPING_TOKENS_CONVERT_IMPL,
+)
+
+_TEST_GO_GOQUERY = new_converter_check(
+    "go_goquery",
+    converter=go_goquery,
+    include=REQUIRED_TOKENS_CONVERT_IMPL
+    | CSS_TOKENS_IMPL
+    | TYPING_TOKENS_CONVERT_IMPL,
+    # 1. go structs don't need init constructor
+    # 2. try/catch (default) operation
+    #    realized by defer func() + rescue
+    exclude={TokenType.STRUCT_INIT, TokenType.EXPR_DEFAULT_END},
+)
+
+_TEST_JS_PURE = new_converter_check(
+    "js_pure",
+    converter=js_pure,
+    include=REQUIRED_TOKENS_CONVERT_IMPL | CSS_TOKENS_IMPL | XPATH_TOKENS_IMPL,
+    # pure js dont need imports
+    exclude={TokenType.IMPORTS},
+)
+
+
+@pytest.mark.parametrize(
+    "name,converter,tokens_include,tokens_exclude",
+    [
+        _TEST_BS4,
+        _TEST_PARSEL,
+        _TEST_SELECTOLAX,
+        _TEST_SCRAPY,
+        _TEST_DART,
+        _TEST_GO_GOQUERY,
+        _TEST_JS_PURE,
+    ],
+)
+def test_converter_impl(
+    name: str,
+    converter: BaseCodeConverter,
+    tokens_include: set[TokenType],
+    tokens_exclude: set[TokenType],
+) -> None:
+    def _any_token_impl(token: TokenType) -> bool:
+        """return True if convert token configured"""
+        return converter.pre_definitions.get(
+            token, False
+        ) or converter.post_definitions.get(token, False)  # type: ignore
+
+    for t in tokens_include:
+        if t in tokens_exclude:
+            continue
+        assert _any_token_impl(t), (
+            f"{name}: Token TokenType.{t.name} not implemented"
+        )
