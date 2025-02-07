33701c51e4b1e036603b504bf9291cfb88c4f5a2
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-02-07 22:14:15 +0300

    new tests, refactoring invalid_expr test cases

--- tests/document/test_document_assert.py
+++ tests/document/test_document_assert.py
@@ -1,4 +1,5 @@
 import pytest
+
 from ssc_codegen import D, R
 from ssc_codegen.document import BaseDocument
 
@@ -9,9 +10,16 @@ from ssc_codegen.document import BaseDocument
         D().is_css("a"),
         D().is_xpath("//a"),
         R().is_equal(""),
+        R().to_int().is_equal(0),
+        R().to_float().is_equal(.1),
+        R().is_not_equal(""),
+        R().to_int().is_not_equal(1),
+        R().to_float().is_not_equal(.1),
         R().split(" ").is_contains(""),
         R().is_regex("k"),
+        R().split(" ").to_int().is_contains(1),
+        R().split(" ").to_float().is_contains(0.1),
     ],
 )
 def test_assert_expr(expr: BaseDocument) -> None:
-    assert True
+    assert expr
--- tests/document/test_document_invalid_expr.py
+++ tests/document/test_document_invalid_expr.py
@@ -1,58 +1,47 @@
+from typing import Callable
+
 import pytest
-from ssc_codegen import D, N, R, ItemSchema
+
+from ssc_codegen import D, Document, ItemSchema, N, R
 
 
 class MockParser(ItemSchema):
     pass
 
 
-def test_raw_invalid_expr() -> None:
+@pytest.mark.parametrize(
+    "document",
+    [
+        # NOTE: lambda wrapper needed for avoid throw error in python compile bytecode stage
+        lambda: R().css("a"),
+        lambda: R().xpath("//a"),
+        lambda: R().css_all("a"),
+        lambda: R().xpath_all("//a"),
+        lambda: N().sub_parser(MockParser).css("a"),
+        lambda: N().sub_parser(MockParser).sub_parser(MockParser),
+        lambda: D().trim(""),
+        lambda: D().index(0),
+        lambda: D().is_regex(""),
+        lambda: D().css("a").default(None),
+        lambda: D().is_equal(""),
+        lambda: D().is_not_equal(""),
+        lambda: D().is_contains(""),
+        lambda: R().is_css("title"),
+        lambda: R().is_xpath("//title"),
+        lambda: R().split(" ").is_contains(1),
+        lambda: R().split(" ").is_contains(0.1),
+        lambda: R().split(" ").to_int().is_contains(0.1),
+        lambda: R().split(" ").to_int().is_contains("a"),
+        lambda: R().split(" ").to_float().is_contains(1),
+        lambda: R().split(" ").to_float().is_contains("a"),
+        lambda: R().is_equal(1),
+        lambda: R().is_equal(0.1),
+        lambda: R().to_int().is_equal(0.1),
+        lambda: R().to_int().is_equal("a"),
+        lambda: R().to_float().is_equal(1),
+        lambda: R().to_float().is_equal("a"),
+    ],
+)
+def test_invalid_expr(document: Callable[[], Document]) -> None:
     with pytest.raises(SyntaxError):
-        R().css("a")
-
-    with pytest.raises(SyntaxError):
-        R().xpath("a")
-
-    with pytest.raises(SyntaxError):
-        R().xpath_all("a")
-
-    with pytest.raises(SyntaxError):
-        R().css_all("a")
-
-
-def test_nested_invalid_expr() -> None:
-    with pytest.raises(SyntaxError):
-        N().sub_parser(MockParser).css("a")
-
-    with pytest.raises(SyntaxError):
-        N().sub_parser(MockParser).sub_parser(MockParser)
-
-
-def test_invalid_doc_expr() -> None:
-    with pytest.raises(SyntaxError):
-        D().trim("")
-
-    with pytest.raises(SyntaxError):
-        D().index(0)
-
-    with pytest.raises(SyntaxError):
-        D().is_regex("")
-
-    with pytest.raises(SyntaxError):
-        D().css("a").default(None)
-
-    with pytest.raises(SyntaxError):
-        D().is_equal("")
-
-    with pytest.raises(SyntaxError):
-        D().is_not_equal("")
-
-    with pytest.raises(SyntaxError):
-        D().is_contains("")
-
-    with pytest.raises(SyntaxError):
-        R().is_css("title")
-
-    with pytest.raises(SyntaxError):
-        R().is_xpath("//title")
-
+        document()
