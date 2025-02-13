f6a2e022be4f84dfcc5dbcdd2e167aee16c010e4
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-02-14 01:08:40 +0300

    add generate schemas from json response example, fix typo

--- examples/tutorial/7_json_extract/schema.py
+++ examples/tutorial/7_json_extract/schema.py
@@ -1,21 +1,23 @@
-from ssc_codegen import ItemSchema, R, Json, D
+# you can generate json template structs from original json:
+#  ssc-gen json-gen raw_json.json -o template_json_schema.py
+# from stdin:
+# echo '{"a": ["b", "c"], "attributes": {"spam": 1.0, "eggs": "foobar", "default": null}}' | ssc-gen json-gen -o template_json_schema.py
+from ssc_codegen import ItemSchema, Json, D
 
 
 # new special marker for json serializations
 class Attributes(Json):
     spam: float
     eggs: str
-    default: str | None
+    default: None
 
 class Content(Json):
     a: list[str]
-    # can be nested
     attributes: Attributes
-
 
 class Main(ItemSchema):
     jsn = (D()
            .css('script').text()
-            # not allowed modify later
-            # accept string only
+            # not allowed to modify later
+            # accept string
            .jsonify(Content))
