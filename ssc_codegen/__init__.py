cae10d769ab71ebacf108844656bedd7a18fd783
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-02-15 00:26:02 +0300

    minor bump

--- ssc_codegen/__init__.py
+++ ssc_codegen/__init__.py
@@ -3,7 +3,7 @@ from .document import HTMLDocument, StringDocument, ArrayDocument, AssertDocumen
 from .json_struct import Json
 from .schema import ItemSchema, DictSchema, ListSchema, FlatListSchema
 
-VERSION = "0.5.6"
+VERSION = "0.6.0"
 
 class __MISSING(object):
     pass
