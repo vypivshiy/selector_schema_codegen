ef57508e4bbc6940e9507abc07be69a40448559a
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-02-18 15:18:12 +0300

    add tests folder format

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -5,8 +5,11 @@ if [ -d '.venv' ] ; then
     export PREFIX=".venv/bin/"
 fi
 export SOURCE_FILES="ssc_codegen"
+export TEST_FILES="tests"
 
 set -x
 
 ${PREFIX}ruff format $SOURCE_FILES
 ${PREFIX}ruff check $SOURCE_FILES --fix
+${PREFIX}ruff format $TEST_FILES
+${PREFIX}ruff check $TEST_FILES --fix
