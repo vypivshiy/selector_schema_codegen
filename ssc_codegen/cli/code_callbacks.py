e186852020da1da6234744137716d1e2670fa65e
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-02-18 15:18:49 +0300

    fix remove_empty_lines flag

--- ssc_codegen/cli/code_callbacks.py
+++ ssc_codegen/cli/code_callbacks.py
@@ -1,8 +1,12 @@
 """"""
 
 from typing import Callable
-from ssc_codegen.str_utils import go_unimport_naive, py_optimize_return_naive, py_str_format_to_fstring, \
-    js_pure_optimize_return
+from ssc_codegen.str_utils import (
+    go_unimport_naive,
+    py_optimize_return_naive,
+    py_str_format_to_fstring,
+    js_pure_optimize_return,
+)
 
 
 class BaseCodeCallback:
@@ -19,13 +23,16 @@ class BaseCodeCallback:
     def __call__(self, lines: list[str]) -> str:
         if self.remove_empty_lines:
             code = self.join_sep.join([i for i in lines if i])
-        code = self.join_sep.join(lines)
+        else:
+            code = self.join_sep.join(lines)
         for cb in self.callbacks:
             code = cb(code)
         return code
 
 
-CB_PY_CODE = BaseCodeCallback(py_optimize_return_naive, py_str_format_to_fstring)
+CB_PY_CODE = BaseCodeCallback(
+    py_optimize_return_naive, py_str_format_to_fstring
+)
 CB_GO_CODE = BaseCodeCallback(go_unimport_naive, remove_empty_lines=True)
 CB_DART_CODE = BaseCodeCallback(remove_empty_lines=True)
 CB_JS_CODE = BaseCodeCallback(js_pure_optimize_return)
