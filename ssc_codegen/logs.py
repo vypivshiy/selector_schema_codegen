f8554f420085e3a1faeb8417b3109f6b905368d8
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-03-23 16:43:18 +0300

    ruff fmt

--- ssc_codegen/logs.py
+++ ssc_codegen/logs.py
@@ -4,30 +4,30 @@ import sys
 
 class ColoredFormatter(logging.Formatter):
     COLOR_MAP = {
-        'DEBUG': "\033[94m",  # Blue
-        'INFO': "\033[92m",  # Green
-        'WARNING': "\033[93m",  # Yellow
-        'ERROR': "\033[91m",  # Red
-        'CRITICAL': "\033[95m",  # Magenta
-        'RESET': "\033[0m"  # Reset
+        "DEBUG": "\033[94m",  # Blue
+        "INFO": "\033[92m",  # Green
+        "WARNING": "\033[93m",  # Yellow
+        "ERROR": "\033[91m",  # Red
+        "CRITICAL": "\033[95m",  # Magenta
+        "RESET": "\033[0m",  # Reset
     }
 
     def format(self, record: logging.LogRecord) -> str:
         if color := self.COLOR_MAP.get(record.levelname):
-            reset = self.COLOR_MAP['RESET']
+            reset = self.COLOR_MAP["RESET"]
         else:
             color = ""
             reset = ""
         msg = super().format(record)
-        return f'{color}{msg}{reset}'
+        return f"{color}{msg}{reset}"
 
 
 def setup_logger(
-        name: str="ssc_gen",
-        level: int=logging.INFO,
-        colored: bool=True,
-        fmt: str='[%(levelname)-8s] %(name)s: %(asctime)s - %(message)s',
-        datefmt: str = '%Y-%m-%d %H:%M:%S'
+    name: str = "ssc_gen",
+    level: int = logging.INFO,
+    colored: bool = True,
+    fmt: str = "[%(levelname)-8s] %(name)s: %(asctime)s - %(message)s",
+    datefmt: str = "%Y-%m-%d %H:%M:%S",
 ) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(level)
@@ -38,6 +38,7 @@ def setup_logger(
         if colored:
             if sys.platform.startswith("win"):
                 import colorama
+
                 colorama.init()
             formatter = ColoredFormatter(fmt, datefmt=datefmt)
         else:
@@ -46,10 +47,3 @@ def setup_logger(
         logger.addHandler(handler)
 
     return logger
-
-
-if __name__ == "__main__":
-    logger_ = setup_logger(level=logging.DEBUG)
-    logger_.debug("test foobar")
-    logger_.info("Logger is set up and running!")
-    logger_.warning("test123")
