33898898880182149cde43cfaa74d3560111fd38
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-03-25 01:38:42 +0300

    update example

--- examples/booksToScrape/runtime_compile.py
+++ examples/booksToScrape/runtime_compile.py
@@ -1,16 +1,16 @@
 # EXPERIMENTAL FEATURE: compile ssc_gen config in runtime and run
 
 from ssc_codegen.compiler import Compiler
-from ssc_codegen.converters.py_parsel import converter
+from ssc_codegen.converters.py_parsel import CONVERTER
 import httpx
 import pprint
 
 
 if __name__ == '__main__':
-    compiler = Compiler.from_file("schemas/booksToScrape.py", converter=converter)
+    compiler = Compiler.from_file("schemas/booksToScrape.py", converter=CONVERTER)
     resp = httpx.get("https://books.toscrape.com/")
 
     pprint.pprint(
-        compiler.run_parse("CataloguePagef", resp.text),
+        compiler.run_parse("CataloguePage", resp.text),
         compact=True,
     )
