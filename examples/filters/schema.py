c4ba8b5bd1f5489573323ce09a90f80593919797
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-04-10 14:34:15 +0300

    update to more real example

--- examples/filters/schema.py
+++ examples/filters/schema.py
@@ -1,8 +1,21 @@
-from ssc_codegen import ItemSchema, R, F, D
+from ssc_codegen import ItemSchema, F, D
 
+
+# base logic operators
+# | - OR
+# & - AND
+# ~ - NOT
 
 class FilterSchema(ItemSchema):
-    js_hrefs = R().re_all(r"((?:https?:)/?/[^>]+\.js)").filter(
-        F().starts("https").re("abc") & ~F().re('.*?')
+    urls = D().css_all("a[href]::attr(href)").filter(
+        # value ends .js suffix
+        # OR NOT EQUAL collections of substrings
+        # AND NOT starts '#'
+        F().ends(".js")
+        # same as `F().ne('', '#', "/")`
+        # syntax sugar: allowed pass str or sequence of str
+        # don't remember to wrap it in parens
+        # v---------------------v
+        | (F() != ('', '#', "/"))
+        & ~F().starts("#")
     )
-    title = D().css("title::text")
