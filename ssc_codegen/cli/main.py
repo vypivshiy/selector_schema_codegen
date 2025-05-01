a0c4c80f4e81fe8d2e4f9a769eca578436bace06
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-05-01 12:54:23 +0300

    ruff fix

--- ssc_codegen/cli/main.py
+++ ssc_codegen/cli/main.py
@@ -216,8 +216,8 @@ def gen_js(
     converter = import_converter(f"js_{lib.value}")
     if fmt:
         commands: list[str] = [
-            'prettier --write --tab-width 4 --single-quote --trailing-comma all --semi {}',
-            ]
+            "prettier --write --tab-width 4 --single-quote --trailing-comma all --semi {}",
+        ]
         fmt_cmd = create_fmt_cmd(ssc_files, prefix, suffix, out, commands)
     else:
         fmt_cmd = []
