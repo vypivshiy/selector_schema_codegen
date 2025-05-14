1438fc4c290fa1a47bfbf603b1104bf01e269f02
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-05-14 11:15:52 +0300

    add deprecated decorator

--- ssc_codegen/_compat.py
+++ ssc_codegen/_compat.py
@@ -1,4 +1,7 @@
 import sys
+import warnings
+import functools
+
 
 if sys.version_info >= (3, 11):
     from enum import StrEnum
@@ -7,3 +10,20 @@ else:
 
     class StrEnum(str, Enum):  # noqa
         pass
+
+
+
+def deprecated(reason=""):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            warnings.warn(
+                f"Function {func.__name__} is deprecated. {reason}",
+                category=DeprecationWarning,
+                stacklevel=2,
+            )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
