21cca9b3eea9fd291ac80435791d6177618d4b12
georgiy <vypivshiy@shitposting.expert>
2025-11-06 12:13:02 +0300

    python: add flag option: remove backport functions str.removeprefix() str.removesuffix()

--- ssc_codegen/cli/ast_grep.py
+++ ssc_codegen/cli/ast_grep.py
@@ -4,4 +4,5 @@ from importlib import resources
 RULES_PATH = resources.files("ssc_codegen.ast_grep_rules")
 
 PY_RULES = str(RULES_PATH.joinpath("py_rules.yml"))
+PY_NEW_RM_PREFIX_SUFFIX_SYNTAX = str(RULES_PATH.joinpath("py_drop_prefix_suffix_backport.yml"))
 JS_RULES = str(RULES_PATH.joinpath("js_rules.yml"))
